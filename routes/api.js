17f52f80ecc5cbf8297c2ee88bb51b0fccbd893d
tonocm <tonocm94@hotmail.com>
2015-09-05 10:49:33 -0400

    integrating 4square

--- routes/api.js
+++ routes/api.js
@@ -61,8 +61,8 @@ router.get('/get/location/photos', function(req, res){
 
             ret_json = body;
             console.log(ret_json);
-            console.log(body['venues']);
-            if (body.venues[0].id === undefined){
+            console.log(body.response.venues[0].id);
+            if (body.response.venues[0].id === undefined){
                 res.send('No results found. Please try again');
             }
             else{
