f729d2056fae47b0160d36a7a5206013f086f455
tonocm <tonocm94@hotmail.com>
2015-09-04 22:42:57 -0400

    started api

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('this is an api!');
+  res.send('<h1>Welcome!</h1>');
 });
 
 module.exports = router;
