45562cd372598f5aa3d3b937711bed36e30aa224
tonocm <tonocm94@hotmail.com>
2015-09-05 16:57:08 -0400

    habilitating post requests

--- routes/api.js
+++ routes/api.js
@@ -57,7 +57,7 @@ router.get('/get/location/photos', function(req, res){
             var ret_json = JSON.parse(body);
             var first_venue = ret_json.response.venues[0];
 
-            if (first_venue.id === undefined){
+            if (first_venue === undefined){
                 res.send('No results found. Please try again');
             }
             else{
