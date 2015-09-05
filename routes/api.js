0dd70882b39cfded3d46280f892061a2c5dfaf9d
tonocm <tonocm94@hotmail.com>
2015-09-05 03:48:13 -0400

    addming mongodb

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
 
 /* GET users listing. */
 router.get('/', function(req, res) {
@@ -10,8 +13,12 @@ router.get('/get/properties', function(req, res) {
   res.json({'name':'Wells Fargo Center', 'cost':'10,000', 'owner':'Comcast Spectacor'});
 });
 
-router.get('/get/user', function(req, res) {
-  res.send('<h1>Scouter!</h1>');
+router.get('/get/db', function(req, res) {
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function (callback) {
+    res.send("working!");
+  });
 });
 
 module.exports = router;
