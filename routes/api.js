d15bfa9e54a183d26a3ba131fe9e7cf24fdcde5a
tonocm <tonocm94@hotmail.com>
2015-09-04 22:48:35 -0400

    started api

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('<h1>Welcome!</h1>');
+  res.send('<h1>Scouter!</h1>');
+});
+
+router.get('/get/properties', function(req, res) {
+  res.json({'name':'Wells Fargo Center', 'cost':'10,000', 'owner':'Comcast Spectacor'});
+});
+
+router.get('/get/user', function(req, res) {
+  res.send('<h1>Scouter!</h1>');
 });
 
 module.exports = router;
