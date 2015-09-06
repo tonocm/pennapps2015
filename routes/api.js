d71f674f73c97317c103a025594756b139e5be65
tonocm <tonocm94@hotmail.com>
2015-09-05 23:33:06 -0400

    search by location

--- routes/api.js
+++ routes/api.js
@@ -210,7 +210,7 @@ router.get('/locations/:id', function(req, res) {
   return request(ELASTICSEARCH + '/locations/location/' + req.params.id, function(error, response, body) {
     if (!body.error) {
 
-      console.log(JSON.render(body));
+      console.log(JSON.parse(body));
 
       res.json({
         data: body._source(function (location) {
