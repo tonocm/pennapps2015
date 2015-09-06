54031221d544491e80503dbb5d7257598a46ffb1
tonocm <tonocm94@hotmail.com>
2015-09-06 06:20:12 +0000

    finalized version

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ app.use(function(err, req, res, next) {
 });
 
 // Start the server
-var server = app.listen(process.env.PORT || 3000, function () {
+var server = app.listen(process.env.PORT || 80, function () {
   console.log('Listening on port ' + server.address().port);
 });
